c76a6eb602a7d9263b7829391dfdf0815741bbcf
vaibhavnarkhede12 <39726664+vaibhavnarkhede12@users.noreply.github.com>
2022-05-21 12:12:26 +0530

    Create methodtest.py

---
+++ methodtest.py
@@ -0,0 +1,14 @@
+print(" this is a python code for automated method testing ")
+
+def name():
+    print("vaibhav")
+    
+
+def surname():
+    print("narkhede")
+    
+def friendname():
+    print("xxxx")
+    
+if __name__ == "__main__":
+    name();
